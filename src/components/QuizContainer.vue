b737202ede2fc682ca94662086ce45131097a04f
Zayyad Muhammad Sani <zayyadmuhammed700@gmail.com>
2024-09-03 12:33:12 +0100

    Centered modal

--- src/components/QuizContainer.vue
+++ src/components/QuizContainer.vue
@@ -74,10 +74,10 @@
         </div>
         <!-- Game over Modal -->
         <div class="modal fade" id="gameOverModal" tabindex="-1" aria-labelledby="gameOverModalLabel" aria-hidden="true">
-            <div class="modal-dialog">
+            <div class="modal-dialog modal-dialog-centered">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <!-- <h1 class="modal-title fs-5" id="gameOverModalLabel">Game Over!</h1> -->
+                        <h1 class="modal-title fs-5" id="gameOverModalLabel">Game Over!</h1>
                         <!-- <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button> -->
                     </div>
                     <div class="modal-body">
