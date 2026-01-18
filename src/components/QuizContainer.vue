b6a0aa3440ed2df4334921e801e52a48b36b1d82
Zayyad Muhammad Sani <zayyadmuhammed700@gmail.com>
2026-01-18 14:08:25 +0100

    Added Configcat-vue. Handled pause error

--- src/components/QuizContainer.vue
+++ src/components/QuizContainer.vue
@@ -1,5 +1,5 @@
 <script setup>
-    import { ref, onMounted } from 'vue';
+    import { ref, onMounted, onBeforeMount, inject } from 'vue';
     import Question from './Question.vue';
     import { Modal } from 'bootstrap';
     import { useUserStore } from '@/stores/user'; 
@@ -10,13 +10,26 @@
 
     const emit = defineEmits(['generateQuestions']);
 
+    const configCatClient = inject('configCatClient');
+    let isInlineSVGEnabled;
+
     const userStore = useUserStore();
 
     let gameOverModal;
 
+    onBeforeMount(() => {
+        async function enableInlineSVG() {
+            isInlineSVGEnabled = await configCatClient.getValueAsync("inline_svg_enabled", false);
+        }
+        enableInlineSVG();
+    })
+
+
+
     onMounted(() => {
         userStore.setHighScore();
         gameOverModal = new Modal('#gameOverModal');
+        console
     })
 
     let score = 0;
@@ -40,7 +53,7 @@
 
     function loadNextQuestion() {
         // stop playing the audio
-        questionAudio.pause();
+        questionAudio?.pause();
 
         if(questionCounter.value + 1 < props.questions.length) {
             questionCounter.value++;
@@ -90,8 +103,8 @@
         </div>
         <div id="sound__button__container">
             <button id="sound__icon__button" class="btn btn-info" @click="playSound">
-                <!-- <img id="sound__icon" src="/loudspeaker.svg" width="50"/> -->
-                 <svg id="loudspeaker_icon" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="16" height="16" viewBox="0 0 16 16">
+                <img v-if="isInlineSVGEnabled" id="sound__icon" src="/loudspeaker.svg" width="50"/>
+                 <svg v-else id="loudspeaker_icon" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="16" height="16" viewBox="0 0 16 16">
 <path fill="#000000" d="M11.243 12.993c-0.192 0-0.384-0.073-0.53-0.22-0.293-0.293-0.293-0.768 0-1.061 2.047-2.047 2.047-5.378 0-7.425-0.293-0.293-0.293-0.768 0-1.061s0.768-0.293 1.061 0c1.275 1.275 1.977 2.97 1.977 4.773s-0.702 3.498-1.977 4.773c-0.146 0.146-0.338 0.22-0.53 0.22v0zM8.578 11.578c-0.192 0-0.384-0.073-0.53-0.22-0.293-0.293-0.293-0.768 0-1.061 1.267-1.267 1.267-3.329 0-4.596-0.293-0.293-0.293-0.768 0-1.061s0.768-0.293 1.061 0c1.852 1.852 1.852 4.865 0 6.718-0.146 0.146-0.338 0.22-0.53 0.22z"></path>
 <path fill="#000000" d="M6.5 15c-0.13 0-0.258-0.051-0.354-0.146l-3.854-3.854h-1.793c-0.276 0-0.5-0.224-0.5-0.5v-5c0-0.276 0.224-0.5 0.5-0.5h1.793l3.854-3.854c0.143-0.143 0.358-0.186 0.545-0.108s0.309 0.26 0.309 0.462v13c0 0.202-0.122 0.385-0.309 0.462-0.062 0.026-0.127 0.038-0.191 0.038z"></path>
 </svg>
--- src/main.js
+++ src/main.js
@@ -3,9 +3,14 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import "bootstrap";
+import { ConfigCatPlugin } from 'configcat-vue';
 
 const app = createApp(App)
 
 app.use(createPinia())
+
+app.use(ConfigCatPlugin, {
+    sdkKey: import.meta.env.VITE_CONFIGCAT_SDK_KEY,
+});
 
 app.mount('#app')
